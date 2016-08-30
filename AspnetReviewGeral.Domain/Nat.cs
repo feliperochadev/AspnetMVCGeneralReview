719b536d3514cf62531b67e8c3fd974337dffdb2
Felipe Rocha <feliperocha.ti@gmail.com>
2016-08-30 03:19:48 -0300

    add Domain Layer

---
+++ AspnetReviewGeral.Domain/Nat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspnetReviewGeral.Domain
+{
+    public class Nat
+    {
+        public Guid Id { get; set; }
+        public string NatText { get; set; }
+        public DateTime DateCreated { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ AspnetReviewGeral.Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspnetReviewGeral.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string ScreenName { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Bio { get; set; }
+        public IList<Nat> Nats { get; set; }
+        public IList<User> Users { get; set; } 
+    }
+}
